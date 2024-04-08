2c58e03398f23428ad6c42df6bfb139b2aea615d
Edmore Moyo <etmoyo@gmail.com>
2024-04-08 10:41:42 -0400

    gets a new session token

--- taskRunner/post_processor.py
+++ taskRunner/post_processor.py
@@ -4,6 +4,7 @@ from boto3 import client as boto3_client
 # import modules used here -- sys is a very standard one
 import sys
 import os
+import requests
 
 ecs_client = boto3_client("ecs", region_name=os.environ['REGION'])
 
@@ -13,7 +14,29 @@ def main():
     integration_id = sys.argv[1] # pass from gateway
     api_key = sys.argv[2] # pass from gateway, differ per app?
     api_secret = sys.argv[3] # pass from gateway
-    session_token = sys.argv[4] # should get new session token now that an orchestrator calls the post processor
+    # session_token = sys.argv[4] # should get new session token now that an orchestrator calls the post processor
+    # get session_token
+    r = requests.get(f"{pennsieve_host}/authentication/cognito-config")
+    r.raise_for_status()
+
+    cognito_app_client_id = r.json()["tokenPool"]["appClientId"]
+    cognito_region = r.json()["region"]
+
+    cognito_idp_client = boto3_client(
+    "cognito-idp",
+    region_name=cognito_region,
+    aws_access_key_id="",
+    aws_secret_access_key="",
+    )
+            
+    login_response = cognito_idp_client.initiate_auth(
+    AuthFlow="USER_PASSWORD_AUTH",
+    AuthParameters={"USERNAME": api_key, "PASSWORD": api_secret},
+    ClientId=cognito_app_client_id,
+    )
+
+    session_token = login_response["AuthenticationResult"]["AccessToken"]
+    print("token: ", session_token)
 
     task_definition_name = os.environ['TASK_DEFINITION_NAME_POST']
     subnet_ids = os.environ['SUBNET_IDS']
